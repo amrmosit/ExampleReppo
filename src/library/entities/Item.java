012c1c5d0eb5a9c10b0c59b6fb4f6fdf058b216d
Amr Mostafa <amrmos.it@gmail.com>
2022-07-30 19:39:39 +0800

    modified Item to reflect style guidlines

--- src/library/entities/Item.java
+++ src/library/entities/Item.java
@@ -5,99 +5,100 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Item implements Serializable {
 	
-	private ItemType TyPe;
-	private String aUtHoR;
-	private String TiTlE;
-	private String CaLlNo;
-	private long Id;
+	private ItemType type;
+	private String author;
+	private String title;
+	private String callNo;
+	private long id;
 	
-	private enum iTeM_StAtE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
-	private iTeM_StAtE sTaTe;
+	private enum ItemState { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
+	private ItemState state;
 	
 	
 	public Item(String AuThOr, String tItLe, String cAlLnO, ItemType ItEmTyPe, long iD) {
-		this.TyPe = ItEmTyPe;
-		this.aUtHoR = AuThOr;
-		this.TiTlE = tItLe;
-		this.CaLlNo = cAlLnO;
-		this.Id = iD;
-		this.sTaTe = iTeM_StAtE.AVAILABLE;
+		this.type = ItEmTyPe;
+		this.author = AuThOr;
+		this.title = tItLe;
+		this.callNo = cAlLnO;
+		this.id = iD;
+		this.state = ItemState.AVAILABLE;
 	}
 	
 	public String toString() {
 		StringBuilder Sb = new StringBuilder();
-		Sb.append("Item: ").append(Id).append("\n")
-		  .append("  Type:   ").append(TyPe).append("\n")
-		  .append("  Title:  ").append(TiTlE).append("\n")
-		  .append("  Author: ").append(aUtHoR).append("\n")
-		  .append("  CallNo: ").append(CaLlNo).append("\n")
-		  .append("  State:  ").append(sTaTe);
+		Sb.append("Item: ").append(id).append("\n")
+		  .append("  Type:   ").append(type).append("\n")
+		  .append("  Title:  ").append(title).append("\n")
+		  .append("  Author: ").append(author).append("\n")
+		  .append("  CallNo: ").append(callNo).append("\n")
+		  .append("  State:  ").append(state);
 		
 		return Sb.toString();
 	}
 
-	public Long GeTiD() {
-		return Id;
+	public Long getId() {
+		return id;
 	}
 
-	public String GeTtItLe() {
-		return TiTlE;
+	public String getTitle() {
+		return title;
 	}
 
-	public ItemType GeTtYpE() {
-		return TyPe;
+	public ItemType getType() {
+		return type;
 	}
 
 
 	
-	public boolean Is_AvAiLaBlE() {
-		return sTaTe == iTeM_StAtE.AVAILABLE;
+	public boolean isAvailable() {
+		return state == ItemState.AVAILABLE;
 	}
 
 	
-	public boolean Is_On_LoAn() {
-		return sTaTe == iTeM_StAtE.ON_LOAN;
+	public boolean isOnLoan() {
+		return state == ItemState.ON_LOAN;
 	}
 
 	
-	public boolean Is_DaMaGeD() {
-		return sTaTe == iTeM_StAtE.DAMAGED;
+	public boolean isDamaged() {
+		return state == ItemState.DAMAGED;
 	}
 
 	
-	public void TaKeOuT() {
-		if (sTaTe.equals(iTeM_StAtE.AVAILABLE)) 
-			sTaTe = iTeM_StAtE.ON_LOAN;
+	public void taKeOuT() {
+		if (state.equals(ItemState.AVAILABLE))
+			state = ItemState.ON_LOAN;
 		
 		else 
-			throw new RuntimeException(String.format("Item: cannot borrow item while item is in state: %s", sTaTe));
+			throw new RuntimeException(String.format("Item: cannot borrow item while item is in state: %s", state));
 		
 		
 	}
 
 
-	public void TaKeBaCk(boolean DaMaGeD) {
-		if (sTaTe.equals(iTeM_StAtE.ON_LOAN)) 
-			if (DaMaGeD) 
-				sTaTe = iTeM_StAtE.DAMAGED;			
+	public void taKeBaCk(boolean DaMaGeD) {
+		if (state.equals(ItemState.ON_LOAN))
+			if (DaMaGeD) {
+				state = ItemState.DAMAGED;
+			}
 
-			else 
-				sTaTe = iTeM_StAtE.AVAILABLE;		
-
+			else {
+				state = ItemState.AVAILABLE;
+			}
 		
-		else 
-			throw new RuntimeException(String.format("Item: cannot return item while item is in state: %s", sTaTe));
-				
+		else {
+			throw new RuntimeException(String.format("Item: cannot return item while item is in state: %s", state));
+		}
 	}
 
 	
-	public void rEpAiR() {
-		if (sTaTe.equals(iTeM_StAtE.DAMAGED)) 
-			sTaTe = iTeM_StAtE.AVAILABLE;
-		
-		else 
-			throw new RuntimeException(String.format("Item: cannot repair while Item is in state: %s", sTaTe));
-		
+	public void repair() {
+		if (state.equals(ItemState.DAMAGED)) {
+			state = ItemState.AVAILABLE;
+		}
+		else {
+			throw new RuntimeException(String.format("Item: cannot repair while Item is in state: %s", state));
+		}
 	}
 
 
